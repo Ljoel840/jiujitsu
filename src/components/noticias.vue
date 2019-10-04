9f3ebc45135da915461614068df24e3c99a5d04a
ljoel840 <joellealad@gmail.com>
2019-10-03 21:21:22 -0400

    Video, Enlaces de las noticias, texto overflow y ordenar noticias

--- src/components/noticias.vue
+++ src/components/noticias.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container noticias">
-        <h1 v-if="noti">NOTICIAS</h1>
-        <div class="row" v-if="noti">
+
+        <!-- <h1 v-if="noti">NOTICIAS</h1>
+        <div class="row alto" v-if="noti">
             <div class="col-md-4" v-for="not in noti" :key="not.id">
                 <div class="row noticia">
                     <div class="contenedorTitulo"><h4 class="tituloNoticia"> {{not.titulo}} </h4> </div>
@@ -11,7 +12,30 @@
                 </div>
             </div>
 
+        </div> -->
+    
+    <h1 v-if="noti">NOTICIAS</h1>
+        <div class="row alto" v-if="noti">
+            <div class="col-md-4" v-for="not in noti" :key="not.id">
+                <div class="row noticia">
+                    <div class="contenedorTitulo"><h4 class="tituloNoticia"> {{not.titulo}} </h4> </div>
+                    <div class="contenido">
+                        <video v-if="not.video.activo" width="90%" controls>
+                             <source :src="not.video.url" type="video/mp4">
+                                Your browser does not support HTML5 video.
+                        </video>
+                        <img class="fotoNoticia" v-if="not.imagen.activo && !not.boton.accion "  :src="not.imagen.url" alt="foto noticia">
+                        <a v-if="not.boton.accion" :href="not.boton.accion" target="_blank">
+                            <img class="fotoNoticia" v-if="not.imagen.activo"  :src="not.imagen.url" alt="foto noticia">
+                        </a>
+                        <p class="textoNoticia" v-if="not.parrafos[0]">{{not.parrafos[0].texto}}</p>
+                        <p class="textoNoticia" v-if="not.parrafos[1]">{{not.parrafos[1].texto}}</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
 
     </div>
 </template>
@@ -23,8 +47,10 @@ export default {
     data () {
         return {
             noti:'',
+            arregloOrdenado: []
         }
     },
+    
     methods: {
   
         async obtenerDatos(){
@@ -34,18 +60,35 @@ export default {
                 pagina: '/'
             })
             .then (response=>{
-                console.log(response.data.frontBlogs);
                 this.noti = response.data.frontBlogs;
+                console.log(this.noti);
+                this.arregloOrdenado = this.noti;
+                this.arregloOrdenado.sort(function(a,b) {
+                    return (b.orden - a.orden)
+                })
+                this.noti = this.arregloOrdenado;
+                
             })
             .catch(error=>{
                 console.log(error);
                 this.noti = '';
+                
+
             })
             
+        },
+
+        ordenarDatos(){
+//             this.arregloOrdenado.sort(function (a, b){
+//               return (b.orden - a.orden)
+// })
         }
+      
     },
     created(){
         this.obtenerDatos()
+        this.ordenarDatos()
+        
     }
 }
 </script>
@@ -66,6 +109,12 @@ export default {
         border-radius: 10px;
         margin: 10px 0;
         justify-content: center;
+        
+    }
+    .contenido {
+        height:350px;
+        text-align: center;
+        overflow-y: auto;
     }
     .contenedorTitulo {
         border-radius: 10px 10px 0 0;
@@ -96,5 +145,4 @@ export default {
         padding: 10px;
         margin: 0;
     }
-    
 </style>
